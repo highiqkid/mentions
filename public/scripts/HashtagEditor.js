8ad6c938972a6e0d01cabc99bf5ab0a029e8bde4
highiqkid <victor_cheng@yahoo.com>
2016-06-01 19:10:43 -0700

    changed to base.editor

--- public/scripts/HashtagEditor.js
+++ public/scripts/HashtagEditor.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Editor, { createEditorStateWithText } from 'draft-js-plugins-editor';
 import createHashtagPlugin from 'draft-js-hashtag-plugin';
+import Base from '../css/base.css'; 
+//I ADDED AN IMPORT
 
 const hashtagPlugin = createHashtagPlugin();
 const plugins = [hashtagPlugin];
@@ -31,7 +33,7 @@ export default class SimpleHashtagEditor extends Component {
 
   render() {
     return (
-      <div className={ editorStyles.editor } onClick={ this.focus }>
+      <div className={ Base.editor } onClick={ this.focus }>
         <Editor
           editorState={this.state.editorState}
           onChange={this.onChange}
